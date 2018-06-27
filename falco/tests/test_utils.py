d5933d78dc8324c324c9fe0020d2111019ca23f1
sdwill <sdwill94@gmail.com>
2018-06-27 17:17:59 -0400

    Add tests for padOrCropEven() function

--- falco/tests/test_utils.py
+++ falco/tests/test_utils.py
@@ -15,4 +15,20 @@ def test_ceil_odd():
     assert utils.ceil_odd(3.14159)==5
     assert utils.ceil_odd(2001)==2001
 
+def test_padOrCropEven():
+    # Test case parameters
+    sizes = [2, 10, 98]
+    pads = [2, 6, 8, 10, 40, 98, 256]
+
+    for size in sizes:
+        for pad in pads:
+            input = np.ones((size, size), dtype=np.complex128)
+            output = utils.padOrCropEven(input, pad)
+
+            assert output.shape == (pad, pad)  # Check correct shape
+
+            if pad > size:  # If padding, check that output is padded equally on all sides
+                assert np.allclose(output, output[::-1, ::-1])
+            elif pad == size:  # If input and output are same size, make sure input is unmodified
+                assert np.allclose(input, output)
 
