294627355809f078b3be8cf8aa1398914e2ecb1a
sdwill <sdwill94@gmail.com>
2018-07-03 20:10:34 -0400

    Refactor electric field estimation into single function
    The MATLAB version of FALCO contains separate functions for electric
    field estimation using the full and compact models, which differ only in
    the size of the return arrays and the model used to obtain the electric
    field.  These two functions have been combined into a single function
    that uses a keyword option to switch between the two cases.

--- falco/utils.py
+++ falco/utils.py
@@ -106,7 +106,7 @@ def allcomb(*args, **kwargs):
     return list(itertools.product(*args))
 
 
-def falco_est_perfect_Efield_full(mp, DM):
+def falco_est_perfect_Efield(mp, DM, which_model='full'):
     """
     Function to return the perfect-knowledge E-field and summed intensity for the full model.
 
@@ -116,6 +116,9 @@ def falco_est_perfect_Efield_full(mp, DM):
         Parameter structure for current model.
     DM : DeformableMirrorParameters (placeholder class for now)
         Parameter structure for deformable mirrors
+    which_model: string
+        Coronagraph model used to compute the detector-plane electric field. Either 'full' or
+        'compact'.
     Returns
     -------
     Emat : np.ndarray
@@ -124,20 +127,33 @@ def falco_est_perfect_Efield_full(mp, DM):
         Total intensity inside dark hole
     """
 
-    Icube = np.zeros((mp.F4.full.Neta, mp.F4.full.Nxi, mp.Nttlam), dtype=np.float64)
-    Emat = np.zeros((mp.F4.full.corr.inds.shape[0], mp.Nttlam), dtype=np.float64)
+    if which_model == 'full':
+        Icube_shape = (mp.F4.full.Neta, mp.F4.full.Nxim, mp.Nttlam)
+        Emat_shape = (mp.F4.full.corr.inds.shape[0], mp.Nttlam)
+        model = models.model_full
+    elif which_model == 'compact':
+        Icube_shape = (mp.F4.compact.Neta, mp.F4.compact.Nxim, mp.Nttlam)
+        Emat_shape = (mp.F4.compact.corr.inds.shape[0], mp.Nttlam)
+        model = models.model_compact
+    else:
+        raise ValueError('Invalid model specified.  Try \'full\' or \'compact\'.')
 
+    Icube = np.zeros(Icube_shape, dtype=np.float64)
+    Emat = np.zeros(Emat_shape, dtype=np.float64)
+
+    # Initialize model variable dictionary
     modvar = {
         'flagCalcJac': 0,
         'wpsbpIndex': mp.wi_ref,
         'whichSource': 'star'
     }
 
+    # Execute model to obtain electric field and total intensity
     for tsi in range(mp.Nttlam):
         modvar['sbpIndex'] = mp.Wttlam_si[tsi]
         modvar['ttIndex'] = mp.Wttlam_ti[tsi]
 
-        E2D = models.model_full(mp, DM, modvar)
+        E2D = model(mp, DM, modvar)
         Emat[:, tsi] = E2D[mp.F4.corr.inds]  # Exact field inside estimation area
         Icube[:, :, tsi] = (np.abs(E2D) ** 2) * mp.WttlamVec(tsi) / mp.Wsum
 
