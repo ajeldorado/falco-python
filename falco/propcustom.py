5837c5b43ec004fd46d05ac3e8ffa580750c26c5
Scott Will <sdwill94@gmail.com>
2018-08-20 19:04:30 -0400

    Add propcustom_PTP() function

--- falco/propcustom.py
+++ falco/propcustom.py
@@ -1,4 +1,8 @@
 import numpy as np
+import logging
+from falco import utils
+
+log = logging.getLogger(__name__)
 
 
 def propcustom_2FT(E_in, centering='pixel'):
@@ -36,3 +40,48 @@ def propcustom_2FT(E_in, centering='pixel'):
         E_out = np.roll(E_in, (1, 1), axis=(0, 1))  # Move the DC pixel back to the right place
 
     return E_out
+
+
+def propcustom_PTP(E_in, full_width, lambda_, dz):
+    """
+    Propagate an electric field array using the angular spectrum technique.
+
+    Parameters
+    ----------
+    E_in : array_like
+        Square (i.e. NxN) input array.
+    full_width : float
+        The width along each side of the array [meters]
+    lambda_ : float
+        Propagation wavelength [meters]
+    dz : float
+        Propagation distance [meters]
+
+    Returns
+    -------
+    array_like
+        Field after propagating over distance dz.
+
+    """
+    M, N = E_in.shape
+    dx = full_width / N
+    N_critical = int(np.floor(lambda_ * np.abs(dz) / (dx ** 2)))  # Critical sampling
+
+    if M != N:  # Input array is not square
+        raise ValueError('propcustom_PTP: input array is not square')
+
+    elif N < N_critical:
+        log.warning(
+             '''
+             Input array is undersampled.
+                Minimum required samples:  {}
+                                  Actual:  {}
+             '''.format(N_critical, N))
+
+    fx = np.arange(-N // 2, N // 2) / full_width
+    rho = utils.radial_grid(fx)  # Spatial frequency coordinate grid
+
+    kernel = np.fft.fftshift(np.exp(-1j * np.pi * lambda_ * dz * (rho ** 2)))
+    intermediate = np.fft.fftn(np.fft.fftshift(E_in))
+
+    return np.fft.ifftshift(np.fft.ifftn(kernel * intermediate))
