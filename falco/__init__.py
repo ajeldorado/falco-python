aae1a25840fefe5f66ab4f87fcf817d64512f902
Santos F Fregoso <sfregoso@jpl.nasa.gov>
2019-07-24 17:28:37 -0700

    Added dms.py

--- falco/__init__.py
+++ falco/__init__.py
@@ -3,3 +3,4 @@ from .utils import *
 from .wfsc  import *
 from .configs  import *
 from .imaging import *
+from .dms import *
---
+++ falco/dms.py
@@ -0,0 +1,6 @@
+
+def falco_gen_dm_surf(dm,dx,N):
+    pass
+
+def falco_gen_EHLC_FPM_surf_from_cube(dm,flagModel):
+    pass
