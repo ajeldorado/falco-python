e8d49ec59b4186fc740bf2b688d303d3ecb31ab4
Santos F Fregoso <sfregoso@jpl.nasa.gov>
2019-10-17 14:12:28 -0700

    Added tests directory with first unit test file

---
+++ tests/test_units.py
@@ -0,0 +1,27 @@
+import pytest
+import sys
+sys.path.insert(0,"../")
+import falco
+
+class TestClass:
+
+    @classmethod
+    def setup_class(cls):
+        print("Setup TestClass!")
+
+
+    @classmethod
+    def teardown_class(cls):
+        print("Teardown TestClass!")
+
+
+    def test_CanImportModelParameters(cls):
+        pass
+
+    def test_CanCreateMpObject(cls):
+   
+        """ Test creation of MP object """
+        mp = falco.config.ModelParameters()
+        assert mp is not None
+
+        pass
