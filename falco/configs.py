6aab1116345de7c6c620c50c6c0926d7b742af81
A J Eldorado Riggs <ajeldorado@users.noreply.github.com>
2019-10-18 12:26:15 -0700

    Setting up for adding more features.

--- falco/configs.py
+++ falco/configs.py
@@ -1,25 +1,6 @@
 import falco
 import numpy as np
     
-def falco_config_gen_FPM_FOHLC(mp):
-    """
-    Quick Description here
-
-    Detailed description here
-
-    Parameters
-    ----------
-    mp: falco.config.ModelParameters
-        Structure of model parameters
-    Returns
-    -------
-    TBD
-        Return value descriptio here
-    """
-
-    if type(mp) is not falco.config.ModelParameters:
-        raise TypeError('Input "mp" must be of type ModelParameters')
-    pass
 def falco_config_gen_FPM_HLC(mp):
     """
     Quick Description here
