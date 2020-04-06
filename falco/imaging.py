Corrected rotation direction of bowtie LS generation

falco/imaging.py
@@ -75,19 +75,20 @@ def falco_get_PSF_norm_factor(mp):
                     mp.Fend.full.I00[si, wi] = (np.abs(Efull)**2).max()
     
     #--Visually verify the normalized coronagraphic PSF
-    modvar = falco.config.Object() # reset
-    modvar.ttIndex = 1
-    modvar.sbpIndex = mp.si_ref
-    modvar.wpsbpIndex = mp.wi_ref
-    modvar.whichSource = 'star'  
-    E0c = falco.model.compact(mp, modvar)
-    I0c = np.abs(E0c)**2
     if(mp.flagPlot):
+        modvar = falco.config.Object() # reset
+        modvar.ttIndex = 1
+        modvar.sbpIndex = mp.si_ref
+        modvar.wpsbpIndex = mp.wi_ref
+        modvar.whichSource = 'star'  
+        E0c = falco.model.compact(mp, modvar)
+        I0c = np.abs(E0c)**2
+    
         plt.figure(501); plt.imshow(np.log10(I0c)); plt.colorbar();
         plt.title('(Compact Model: Normalization Check Using Starting PSF)'); plt.pause(1e-2)
-    E0f = falco.model.full(mp, modvar)
-    I0f = np.abs(E0f)**2
-    if(mp.flagPlot):
+       
+        E0f = falco.model.full(mp, modvar)
+        I0f = np.abs(E0f)**2
         plt.figure(502); plt.imshow(np.log10(I0f)); plt.colorbar();
         plt.title('(Full Model: Normalization Check Using Starting PSF)'); plt.pause(1e-2)
 

falco/setup.py
@@ -1423,8 +1423,17 @@ def falco_set_initial_Efields(mp):
     mp.Eplanet = mp.P1.full.E; #--Initialize the input E-field for the planet at the entrance pupil. Will apply the phase ramp later
     
     if not hasattr(mp.P1.compact,'E'):
-        mp.P1.compact.E = np.ones((mp.P1.compact.Narr,mp.P1.compact.Narr,mp.Nsbp),dtype=complex)
-    mp.sumPupil = np.sum(np.sum(np.abs(mp.P1.compact.mask*falco.utils.padOrCropEven(np.mean(mp.P1.compact.E,2),mp.P1.compact.mask.shape[0] ))**2)); #--Throughput is computed with the compact model
+        mp.P1.compact.E = np.ones((mp.P1.compact.Narr, mp.P1.compact.Narr, mp.Nsbp), dtype=complex)
+    else:
+        if not mp.P1.compact.E.shape[0] == mp.P1.compact.Narr:
+            EcubeTemp = copy.copy(mp.P1.compact.E)
+            mp.P1.compact.E = np.ones((mp.P1.compact.Narr, mp.P1.compact.Narr, mp.Nsbp), dtype=complex)
+            for si in range(mp.Nsbp):
+                mp.P1.compact.E[:, :, si] = falco.utils.pad_crop(EcubeTemp[:, :, si], (mp.P1.compact.Narr, mp.P1.compact.Narr))
+                pass
+            pass
+        pass
+    mp.sumPupil = np.sum(np.sum(np.abs(mp.P1.compact.mask*falco.utils.padOrCropEven(np.mean(mp.P1.compact.E, 2),mp.P1.compact.mask.shape[0] ))**2)); #--Throughput is computed with the compact model
     
     pass
                 