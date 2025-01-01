0cf9f9060b144b2dccb69d4a4c8eae0f4d17cb6a
Greg Allan <gregory.w.allan@jpl.nasa.gov>
2024-12-31 16:28:09 -0800

    improve testing for fourier_resample

--- tests/unit/test_mft2.py
+++ tests/unit/test_mft2.py
@@ -11,8 +11,7 @@ import matplotlib.pyplot as plt
 
 import falco
 
-def gen_test_pattern():
-    dimsTest = 100
+def gen_test_pattern(dimsTest):
 
     x = falco.util.create_axis(dimsTest, 1)
     y = falco.util.create_axis(dimsTest, 1)
@@ -32,47 +31,194 @@ def gen_test_pattern():
         
         
 
-def test_fourier_resample(test_input):
-    zoom = 1.51
-    fprime = falco.diff_dm.fourier_resample(test_input,zoom)
+def test_fourier_resample_even_to_odd(plot=False):
+    
+    Ninit = 100
+    Nfinal = 151
+    zoom = Nfinal/Ninit
+    f = gen_test_pattern(Ninit)
+    
+    fprime = falco.diff_dm.fourier_resample(f,zoom)
     fprimeprime = falco.diff_dm.fourier_resample(fprime,1/zoom)
     
-    abs_tol = 0.005*np.max(test_input)
-
-    plt.figure()
-    plt.imshow(test_input)
-    plt.colorbar()
-    plt.title('Input')
+    abs_tol = 0.005*np.max(f)
     
-    plt.figure()
-    plt.imshow(fprime)
-    plt.colorbar()
-    plt.title('Output1')
+    if plot:
+        plt.figure()
+        plt.imshow(f)
+        plt.colorbar()
+        plt.title('Input')
+        
+        plt.figure()
+        plt.imshow(fprime)
+        plt.colorbar()
+        plt.title('Output1')
+        
+        plt.figure()
+        plt.imshow(fprimeprime)
+        plt.colorbar()
+        plt.title('Output2')
+        
+        plt.figure()
+        plt.imshow(fprimeprime-f)
+        plt.colorbar()
+        plt.title('Difference')
     
-    plt.figure()
-    plt.imshow(fprimeprime)
-    plt.colorbar()
-    plt.title('Output2')
-    
-    plt.figure()
-    plt.imshow(fprimeprime-test_input)
-    plt.colorbar()
-    plt.title('Difference')
-
-    plt.show()
-    
-    indMaxf = np.unravel_index(np.argmax(test_input),test_input.shape)
+        plt.show()
+        
+    indMaxf = np.unravel_index(np.argmax(f),f.shape)
     indMaxfp = np.unravel_index(np.argmax(fprime),fprime.shape)
     indMaxfpp = np.unravel_index(np.argmax(fprimeprime),fprimeprime.shape)
     
-    assert( np.all( indMaxf==np.floor(np.array(test_input.shape)/2) ) )
+    assert( np.all( indMaxf==np.floor(np.array(f.shape)/2) ) )
     assert( np.all( indMaxfp==np.floor(np.array(fprime.shape)/2) ) )
     assert( np.all( indMaxfpp==np.floor(np.array(fprimeprime.shape)/2) ) )
     
-    maxAbsDiff = np.max(np.abs(fprimeprime - test_input))
+    maxAbsDiff = np.max(np.abs(fprimeprime - f))
+    assert maxAbsDiff < abs_tol
+    
+def test_fourier_resample_odd_to_odd(plot=False):
+    
+    Ninit = 101
+    Nfinal = 151
+    zoom = Nfinal/Ninit
+    f = gen_test_pattern(Ninit)
+    
+    fprime = falco.diff_dm.fourier_resample(f,zoom)
+    fprimeprime = falco.diff_dm.fourier_resample(fprime,1/zoom)
+    
+    abs_tol = 0.005*np.max(f)
+    
+    if plot:
+        plt.figure()
+        plt.imshow(f)
+        plt.colorbar()
+        plt.title('Input')
+        
+        plt.figure()
+        plt.imshow(fprime)
+        plt.colorbar()
+        plt.title('Output1')
+        
+        plt.figure()
+        plt.imshow(fprimeprime)
+        plt.colorbar()
+        plt.title('Output2')
+        
+        plt.figure()
+        plt.imshow(fprimeprime-f)
+        plt.colorbar()
+        plt.title('Difference')
+    
+        plt.show()
+        
+    indMaxf = np.unravel_index(np.argmax(f),f.shape)
+    indMaxfp = np.unravel_index(np.argmax(fprime),fprime.shape)
+    indMaxfpp = np.unravel_index(np.argmax(fprimeprime),fprimeprime.shape)
+    
+    assert( np.all( indMaxf==np.floor(np.array(f.shape)/2) ) )
+    assert( np.all( indMaxfp==np.floor(np.array(fprime.shape)/2) ) )
+    assert( np.all( indMaxfpp==np.floor(np.array(fprimeprime.shape)/2) ) )
+    
+    maxAbsDiff = np.max(np.abs(fprimeprime - f))
+    assert maxAbsDiff < abs_tol
+    
+def test_fourier_resample_odd_to_even(plot=False):
+    
+    Ninit = 101
+    Nfinal = 150
+    zoom = Nfinal/Ninit
+    f = gen_test_pattern(Ninit)
+    
+    fprime = falco.diff_dm.fourier_resample(f,zoom)
+    fprimeprime = falco.diff_dm.fourier_resample(fprime,1/zoom)
+    
+    abs_tol = 0.005*np.max(f)
+    
+    if plot:
+        plt.figure()
+        plt.imshow(f)
+        plt.colorbar()
+        plt.title('Input')
+        
+        plt.figure()
+        plt.imshow(fprime)
+        plt.colorbar()
+        plt.title('Output1')
+        
+        plt.figure()
+        plt.imshow(fprimeprime)
+        plt.colorbar()
+        plt.title('Output2')
+        
+        plt.figure()
+        plt.imshow(fprimeprime-f)
+        plt.colorbar()
+        plt.title('Difference')
+    
+        plt.show()
+        
+    indMaxf = np.unravel_index(np.argmax(f),f.shape)
+    indMaxfp = np.unravel_index(np.argmax(fprime),fprime.shape)
+    indMaxfpp = np.unravel_index(np.argmax(fprimeprime),fprimeprime.shape)
+    
+    assert( np.all( indMaxf==np.floor(np.array(f.shape)/2) ) )
+    assert( np.all( indMaxfp==np.floor(np.array(fprime.shape)/2) ) )
+    assert( np.all( indMaxfpp==np.floor(np.array(fprimeprime.shape)/2) ) )
+    
+    maxAbsDiff = np.max(np.abs(fprimeprime - f))
+    assert maxAbsDiff < abs_tol
+
+def test_fourier_resample_even_to_even(plot=False):
+    
+    Ninit = 100
+    Nfinal = 150
+    zoom = Nfinal/Ninit
+    f = gen_test_pattern(Ninit)
+    
+    fprime = falco.diff_dm.fourier_resample(f,zoom)
+    fprimeprime = falco.diff_dm.fourier_resample(fprime,1/zoom)
+    
+    abs_tol = 0.005*np.max(f)
+    
+    if plot:
+        plt.figure()
+        plt.imshow(f)
+        plt.colorbar()
+        plt.title('Input')
+        
+        plt.figure()
+        plt.imshow(fprime)
+        plt.colorbar()
+        plt.title('Output1')
+        
+        plt.figure()
+        plt.imshow(fprimeprime)
+        plt.colorbar()
+        plt.title('Output2')
+        
+        plt.figure()
+        plt.imshow(fprimeprime-f)
+        plt.colorbar()
+        plt.title('Difference')
+    
+        plt.show()
+        
+    indMaxf = np.unravel_index(np.argmax(f),f.shape)
+    indMaxfp = np.unravel_index(np.argmax(fprime),fprime.shape)
+    indMaxfpp = np.unravel_index(np.argmax(fprimeprime),fprimeprime.shape)
+    
+    assert( np.all( indMaxf==np.floor(np.array(f.shape)/2) ) )
+    assert( np.all( indMaxfp==np.floor(np.array(fprime.shape)/2) ) )
+    assert( np.all( indMaxfpp==np.floor(np.array(fprimeprime.shape)/2) ) )
+    
+    maxAbsDiff = np.max(np.abs(fprimeprime - f))
     assert maxAbsDiff < abs_tol
 
 
 if __name__ == '__main__':
-    pattern = gen_test_pattern()
-    test_fourier_resample(pattern)
+    plot = True
+    test_fourier_resample_even_to_odd(plot=plot)
+    test_fourier_resample_odd_to_odd(plot=plot)
+    test_fourier_resample_odd_to_even(plot=plot)
+    test_fourier_resample_even_to_even(plot=plot)
