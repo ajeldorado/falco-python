de7ac942e85c700ff0128811d337f30336013524
JoelCourtney <joel.e.courtney@gmail.com>
2025-03-27 16:22:49 -0700

    Rename si -> subband_index

--- falco/config/TestbedInterface.py
+++ falco/config/TestbedInterface.py
@@ -6,6 +6,6 @@ class TestbedInterface(ABC):
     '''An abstract class whose sole method is implemented by the testbed'''
     
     @abstractmethod
-    def get_sbp_image(self,si):
+    def get_sbp_image(self,subband_index):
         pass
     
